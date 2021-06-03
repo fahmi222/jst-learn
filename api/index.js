88f5fe9ca3bf1a0619438ca07a00f7908b188588
fahmi222 <84862720+fahmi222@users.noreply.github.com>
2021-06-03 21:19:49 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -71,6 +71,19 @@ r.get('/predict/:i/:r', function(req, res, next) {
            parseFloat(req.params.i), // string to float
            parseFloat(req.params.r)
        ]
+   ).then((jres_)=>{
+       res.json(jres_)
+    })
+  })
+});
+
+// routers 
+r.get('/classify/:i/:r', function(req, res, next) {
+   model.predict(
+       [
+           parseFloat(req.params.i), // string to float
+           parseFloat(req.params.r)
+       ]
    ).then((jres)=>{
        cls_model.classify(
             [
