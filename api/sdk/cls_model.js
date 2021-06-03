238e265851b8618fe73938d968897d4d09cc154d
fahmi222 <84862720+fahmi222@users.noreply.github.com>
2021-06-04 03:19:17 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -13,12 +13,6 @@ const argMax = argFact((min, el) => (el[0]] > min[0] ? el : min))
 
 function ArgMax(res){
     label = "NORMAL"
-    cls_data = []
-    for(i=0; i<res.length; i++){
-        cls_data[i] = res[i]
-  } 
-  console.log(cls_data, argMax(cls_data));
-  
   if(argMax(cls_data) == 1){
       label = "OVER VOLTAGE"
   }if(argMax(cls_data) == 0){
