dbb8ba671ae40c04ca5e92daa6ec85d8b85a9946
fahmi222 <84862720+fahmi222@users.noreply.github.com>
2021-06-03 18:48:10 +0700

    Create cls_model.js

---
+++ api/sdk/cls_model.js
@@ -0,0 +1,38 @@
+const tf = require('@tensorflow/tfjs-node');
+
+function normalized(data){ // i & r
+    i = (data[0] - 12.585) / 6.813882
+    r = (data[1] - 51.4795) / 29.151289
+    r = (data[2] - 51.4795) / 29.151289
+    r = (data[3] - 51.4795) / 29.151289
+    return [i, r, v, p]
+}
+
+async function predict(data){
+    let in_dim = 4; // i r v p 
+    
+    data = normalized(data);
+    shape = [1, in_dim];
+
+    tf_data = tf.tensor2d(data, shape);
+
+    try{
+        // path load in public access => github
+        const path = 'https://raw.githubusercontent.com/fahmi222/jst-learn/main/public/ex_model/model.json';
+        const model = await tf.loadGraphModel(path);
+        
+        predict = model.predict(
+                tf_data
+        );
+        result = predict.dataSync();
+        return // denormalized( result );
+        
+    }catch(e){
+      console.log(e);
+    }
+}
+
+module.exports = {
+    predict: predict 
+}
+  
