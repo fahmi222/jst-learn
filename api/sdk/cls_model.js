8ea0c5ba630e0b3254985fdf62eec73b88a7c1e3
fahmi222 <84862720+fahmi222@users.noreply.github.com>
2021-06-03 20:54:43 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -19,7 +19,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/fahmi222/jst-learn/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/fahmi222/jst-learn/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
