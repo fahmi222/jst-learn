72a49eed64e94222fba87f000dec072ef4540b9c
fahmi222 <84862720+fahmi222@users.noreply.github.com>
2021-06-04 18:54:54 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -83,7 +83,7 @@ r.get(`/classify/:i/:r`, function(req, res, next) {
    model.predict(
        [
            parseFloat(req.params.i), // string to float
-           parseFloat(req.params.r),
+           parseFloat(req.params.r)
        ]
    ).then((jres)=>{
        cls_model.classify(
